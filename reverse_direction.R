5a4b4041f47df1dfa34de1bb3da893e7e024e1de
aterhorst <andrew.terhorst@gmail.com>
2015-12-06 18:27:00 +1100

    First commit

---
+++ reverse_direction.R
@@ -0,0 +1,18 @@
+#####################################################
+#                                                   #
+#       Function to reverse tie direction           #
+#           provided by Sam Steingold               #
+#                                                   #
+#####################################################
+
+graph.reverse <- function (graph) {
+  if (!is.directed(graph))
+    return(graph)
+  e <- get.data.frame(graph, what="edges")
+  ## swap "from" & "to"
+  neworder <- 1:length(e)
+  neworder[1:2] <- c(2,1)
+  e <- e[neworder]
+  names(e) <- names(e)[neworder]
+  graph.data.frame(e, vertices = get.data.frame(graph, what="vertices"))
+}

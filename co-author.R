9324463ad233a602d4ddf67b7deeeb1b5436ea7e
Andrew Terhorst <Andrew.Terhorst@gmail.com>
2017-06-09 10:01:15 +1000

    co-author update

--- co-author.R
+++ co-author.R
@@ -1,3 +1,10 @@
+#################################################
+#                                               #
+#     R script to create co-author networks     #
+#             Version 2017-06-09                #
+#                                               #
+#################################################
+
 library(readxl)
 library(igraph)
 library(plyr)
@@ -10,24 +17,37 @@ setwd("D:/Andrew/ownCloud/Co-author Network")
 
 # read in CSIRO people data
 
-groups <- read_excel("people_places.xlsx")
+
+## people info
+
+groups <- read_excel("people_places.xlsx") # extracted from PeopleServ.csiro.au
 groups <- subset(groups, select = c("FullName","UserName", "BusinessUnitCode", "LocationCode"))
 groups$BusinessUnitCode <- as.integer(groups$BusinessUnitCode)
 
-org <- read_excel("org_units.xlsx")
+## organisational detail
+
+org <- read_excel("org_units.xlsx") # extracted from OrgUnitServ.csiro.au
 org <- subset(org, select = c("DepartmentCode", "Abbreviation", "LineOfBusiness"))
-colnames(org)[colnames(org)=="DepartmentCode"] <- "BusinessUnitCode"
+colnames(org)[colnames(org) == "DepartmentCode"] <- "BusinessUnitCode"
 org$BusinessUnitCode <- as.integer(org$BusinessUnitCode)
 
+## work location
+
+loc <- read_excel("csiro_location.xlsx") # extracted from LocationServ.csiro.au
+loc <- subset(loc, select = c("Code","City","Country","PostCode"))
+colnames(loc)[colnames(loc) == "Code"] <- "LocationCode"
+
+## combine people, org, location info
 
 groups <- full_join(groups,org, by = "BusinessUnitCode")
-extract <- c("IS", "NF")
-groups <- filter(groups, LineOfBusiness %in% extract)
-groups <- cbind(id = 1:nrow(groups), groups)
+groups <- full_join(groups,loc, by = "LocationCode")
+extract <- c("IS", "NF") # science areas
+groups <- filter(groups, LineOfBusiness %in% extract) # subset according to science areas
+groups <- cbind(id = 1:nrow(groups), groups) # index rows
 
 # import publication data
 
-man <- na.omit(read_excel("ePublish Manuscripts.xls"))
+man <- na.omit(read_excel("ePublish Manuscripts.xls")) # extracted from ePublish.csiro.au
 man$`Publisher Notification Date` <- as.Date(man$`Publisher Notification Date`)
 
 # extract data range
@@ -36,24 +56,24 @@ man_2012_13 <- filter(man, man$`Publisher Notification Date` >= "2012-01-01" & m
 
 # create ragged edge dataframe
 
-aut_2012_13 <- str_split_fixed(man_2012_13$Author, ";", n = 21)
+aut_2012_13 <- str_split_fixed(man_2012_13$Author, ";", n = 160) # max number of co-authors
 
-# reverse lastname, firstname
+# reverse lastname, firstname (thanks to Alex Whan)
 
 rev_name <- function(string, pattern = ", ") {paste(rev(unlist(strsplit(string, pattern))), collapse = " ")} 
-
 aut_2012_13 <- aut_2012_13 %>%  as_data_frame %>%  map_df(map, rev_name) %>%  map_df(unlist)
 
-# trim to 20 co-authors max
+# trim to 20 co-authors max (arbitrary limit)
 
 aut_2012_13 <- as.data.frame(aut_2012_13[,1:20])
 
+# create dyads
 
-# match authors to groups
+## match authors to groups (thanks to Alec Stephenson)
 
-nauthors <- t(apply(aut_2012_13, 1, function(x) match(x, groups$FullName)))
+nauthors <- t(apply(aut_2012_13, 1, function(x) match(x, groups$FullName))) # issue starts here
 
-# generate a clean list of co-authors
+## generate a clean list of co-authors
 
 ind <- which(!is.na(t(nauthors)),arr.ind=TRUE)[,2:1]
 colnames(ind) <- c("row","col")
@@ -61,7 +81,6 @@ trow <- table(ind[,"row"]) # counts number of co-authors per paper
 trow1 <- as.numeric(names(trow[trow==1])) # remove single authors
 ind <- ind[!(ind[,"row"] %in% trow1),] # is a clean list
 ind <- cbind(ind, author = nauthors[ind]) # assign numeric value representing authors and co-authors (from nauthors)
-
 links <- tapply(ind[,"author"], ind[,"row"], function(x) t(combn(x, 2))) # generate list of dyads per paper
 links <- do.call(rbind, links) # generate global list of dyads
 links <- t(apply(links,1,sort)) # order each dyad low to high (first sort)
@@ -71,7 +90,7 @@ links <- matrix(as.character(links), ncol=2) # convert dyads to edge matrix
 
 # undirected graph generated from edge matrix
 
-snet <- graph.edgelist(links, directed=FALSE) 
+co-authornet <- graph.edgelist(links, directed=FALSE) 
 
 # generate attribute info
 

a86afc22d4c3766f03a6b884173289570e43832e
Andrew Terhorst <andrew.terhorst@csiro.au>
2017-06-09 17:58:13 +1000

    fixed

--- co-author.R
+++ co-author.R
@@ -11,18 +11,21 @@ library(plyr)
 library(dplyr)
 library(stringr)
 library(purrr)
+library(ggmap)
+library(RColorBrewer)
+library(reshape)
 
 
-setwd("D:/Andrew/ownCloud/Co-author Network")
+setwd("/OSM/MEL/DPS_OI_Network/work/ownCloud/Co-author Network")
 
 # read in CSIRO people data
-
 
 ## people info
 
 groups <- read_excel("people_places.xlsx") # extracted from PeopleServ.csiro.au
 groups <- subset(groups, select = c("FullName","UserName", "BusinessUnitCode", "LocationCode"))
 groups$BusinessUnitCode <- as.integer(groups$BusinessUnitCode)
+colnames(groups)[colnames(groups) == "UserName"] <- "Ident"
 
 ## organisational detail
 
@@ -45,6 +48,11 @@ extract <- c("IS", "NF") # science areas
 groups <- filter(groups, LineOfBusiness %in% extract) # subset according to science areas
 groups <- cbind(id = 1:nrow(groups), groups) # index rows
 
+## geocode locations
+
+# groups$location <- with(groups, paste0(Country," postcode ", PostCode))
+# groups$geocode <- geocode(groups$location, sensor = FALSE, output = "latlon", source = "google")
+
 # import publication data
 
 man <- na.omit(read_excel("ePublish Manuscripts.xls")) # extracted from ePublish.csiro.au
@@ -62,10 +70,21 @@ aut_2012_13 <- str_split_fixed(man_2012_13$Author, ";", n = 160) # max number of
 
 rev_name <- function(string, pattern = ", ") {paste(rev(unlist(strsplit(string, pattern))), collapse = " ")} 
 aut_2012_13 <- aut_2012_13 %>%  as_data_frame %>%  map_df(map, rev_name) %>%  map_df(unlist)
+aut_2012_13 <- as.data.frame(t(apply(aut_2012_13,1,function(x) gsub("+ "," ",x)))) # fix double white space
+aut_2012_13 <- as.data.frame(t(apply(aut_2012_13,1,function(x) gsub(" +"," ",x)))) # fix leading white space
 
 # trim to 20 co-authors max (arbitrary limit)
 
 aut_2012_13 <- as.data.frame(aut_2012_13[,1:20])
+
+# individual publications stats
+
+simplify <- melt(aut_2012_13, id.vars=0)
+pubtots <- simplify %>% group_by(value) %>% summarise(count = n())
+pubtots$FullName <- as.character(pubtots$FullName)
+colnames(pubtots)[colnames(pubtots) == "value"] <- "FullName"
+colnames(pubtots)[colnames(pubtots) == "count"] <- "Publications"
+groups <- left_join(groups,pubtots, by = "FullName")
 
 # create dyads
 
@@ -90,21 +109,54 @@ links <- matrix(as.character(links), ncol=2) # convert dyads to edge matrix
 
 # undirected graph generated from edge matrix
 
-co-authornet <- graph.edgelist(links, directed=FALSE) 
+g <- graph.edgelist(links, directed=FALSE) 
 
 # generate attribute info
 
-bu <- factor(groups$BusinessUnitCode[as.numeric(V(snet)$name)])
-loc <- factor(groups$LocationCode[as.numeric(V(snet)$name)])
-nme <- factor(groups$FullName[as.numeric(V(snet)$name)])
+bu <- factor(groups$Abbreviation[as.numeric(V(g)$name)])
+place <- factor(groups$LocationCode[as.numeric(V(g)$name)])
+person <- factor(groups$FullName[as.numeric(V(g)$name)])
+prod <- factor(groups$Publications[as.numeric(V(g)$name)])
 
 # assign attributes to vertices
 
-V(snet)$author <- as.character(nme)
-V(snet)$bu <- as.character(div)
-V(snet)$location <- as.character(loc)
+V(g)$author <- as.character(person) # name of person
+V(g)$bu <- as.character(bu) # bu name
+V(g)$location <- as.character(place) # place of work
+V(g)$publications <- as.character(prod) # how many publications produced in period
+
+# sanity check
+
+ego(g,1,nodes = V(g)$author == "Stuart Day", "all") # check against nauthors
+ego(g,1,nodes = V(g)$author == "Raphaele Blanchi", "all")
 
 # export as gml file
 
-write.graph(snet, "co-author.gml", "gml")
+write.graph(g, "co-author.gml", "gml")
+
+# plotting
+
+## extract giant component
+
+cl <- clusters(g) 
+gc <- induced.subgraph(g, which(cl$membership == which.max(cl$csize)))
+
+## configure display parameters
+
+V(gc)$size <- degree(gc)/5
+div <- factor(V(gc)$bu)
+cols <- c("yellow","orange","red","darkred","blue","darkblue","darkgreen","black","lightblue","violet","pink")
+
+## generate graph
+
+pdf("co-author.pdf",width=15,height=15) #call the pdf writer
+plot(gc,vertex.color = cols[as.numeric(bu)], vertex.label=NA, vertex.size=V(gc)$size, edge.width=0.8, layout=layout.kamada.kawai)
+# title(main = "CSIRO co-authorship network 2011-12",cex.main=2.5)
+legend(1,1,legend=levels(div),col=cols, pch = 16, cex=1.25)
+dev.off() #close the device
+
+# export to MPNet
+
+actor_attributes <- get.vertex.attribute(g)
+
 
